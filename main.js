e042eb9852a90aefaadd6e3057c2e05d9fde7b7d
William He <williamhedestiny@gmail.com>
2024-04-17 12:36:16 -0700

    added power up

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 var gameData = {
     plus2: 0,
-    plus2PerClick: 1
+    plus2PerClick: 1,
 }
 
 function plus2() {
     gameData.plus2 += gameData.plus2PerClick
-    document.getElementById("plus2s").innerHTML = gameData.plus2 + "+2s"
+    document.getElementById("plus2s").innerHTML = gameData.plus2 + " +2s"
+}
+
+function plus2add(){
+    gameData.plus2PerClick += 1
+    document.getElementById("plus2PerClick").innerHTML = gameData.plus2PerClick + " +2 per click"
 }
